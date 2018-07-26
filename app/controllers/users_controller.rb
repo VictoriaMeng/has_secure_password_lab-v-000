148e26e02b72d6257888587bfc644ba064570a50
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 23:38:27 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
-
   def create
+    user = User.new(user_params).save
   end
-
-  def show
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
