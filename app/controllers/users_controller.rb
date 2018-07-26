ee39c9dc6172a564ff5a17666bbe2bcfd4aa1bce
Victoria Meng <nights1ststar@protonmail.com>
2018-07-26 23:43:07 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params).save
+    user = User.new(user_params)
+    user.save
     binding.pry
     session[:user_id] = user.id
     redirect_to user_path(user)
